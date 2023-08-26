0aa3cf3938bf979a6a3099b08fd8b7c8b973f566
kuusei <motaro@kuusei.moe>
2023-08-27 00:47:34 +0800

    feat(layout): update support sass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-import "./globals.css";
+import "./globals.scss";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={(inter.className, "flex min-h-screen flex-col items-center justify-between p-24")}>
+      <body className={("flex min-h-screen flex-col items-center justify-between p-24")}>
         {children}
       </body>
     </html>
